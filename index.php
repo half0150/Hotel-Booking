8cfd8104ea863e68d3e8fc5b35a97fd4982b9dda
half0150 <half0150@edu.sde.dk>
2024-07-30 13:20:31 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Monie's Hotel</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="tailwind.config.js">
+    </script>
+</head>
+
+<body class="bg-slate-800">
+    <?php
+    require 'navbar.php';
+    ?>
+    <section class="p-12">
+        <h1 class="text-4xl text-center text-neutral-300 font-bold">Monie's Hotel</h1>
+        <div class="flex justify-center items-center p-24">
+            <img class="shadow-xl" src="images/receptionists-5975962_640.jpg" alt="receptionists">
+        </div>
+    </section>
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,4 @@
+<section class="flex justify-evenly p-4">
+    <h1 class="text-4xl font-bold">Home</h1>
+    <button class="text-4xl font-bold bg-slate-50 rounded-lg">Book Now</button>
+</section>
